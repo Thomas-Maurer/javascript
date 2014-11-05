f440eaf037a61241dd3fe06b224608c3ffd28ecf
Thomas Maurer <thomas.wellan@gmail.com>
2014-11-05 16:38:54 +0100

    Add sexy html + shuffle

--- src/js/Memory.js
+++ src/js/Memory.js
@@ -1,13 +1,14 @@
-// A simple array with constructor.
-var mesImages = new Array( "memory01", "memory02","memory03","memory04","memory05","memory06" );
+// A simple object with constructor.
+var mesImages = new Object({});
+mesImages.addresses = new Array( "memory01", "memory02","memory03","memory04","memory05","memory06");
 //Doc ready
 $( document ).ready(function() {
 	random();
 });
 
 var random = function (){
-	mesImages = shuffle(mesImages);
-	$.each(mesImages, function (index, value) {
+	mesImages.addresses = _.shuffle(mesImages.addresses);
+	$.each(mesImages.addresses, function (index, value) {
   		$( ".row" ).append( "<div class='col-xs-6 col-md-3'><a href='#' class='thumbnail'><img src='css/asset/"+value+".png' alt='...''></a></div>" );
 	});
 };
